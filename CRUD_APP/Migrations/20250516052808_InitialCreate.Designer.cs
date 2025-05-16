c1c0a4f2dac11ade67a5b504b24b9937dca8d533
John <john.koshy@outlook.com>
2025-05-16 23:48:30 +0530

    Updated the CRUD_APP project with SQL Server integration

--- CRUD_APP/Migrations/20250516052808_InitialCreate.Designer.cs
+++ CRUD_APP/Migrations/20250516052808_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using CRUD_APP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CRUD_APP.Migrations
 {
     [DbContext(typeof(CRUD_APPContext))]
-    partial class CRUD_APPContextModelSnapshot : ModelSnapshot
+    [Migration("20250516052808_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
