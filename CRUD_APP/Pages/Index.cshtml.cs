90539f222e6be845e3ec1627bb965b6e72df087e
John <john.koshy@outlook.com>
2025-05-26 15:24:56 +0530

    Unable to solve the build error

--- CRUD_APP/Pages/Index.cshtml.cs
+++ CRUD_APP/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
